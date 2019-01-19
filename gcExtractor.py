9e279e2bdcb9f008286e26e61fcb96ec357f49d4
Jason <linglijunmail@gmail.com>
2019-01-19 12:10:26 +0900

    add test cases.

--- gcExtractor.py
+++ gcExtractor.py
@@ -2,11 +2,18 @@
 # -*- coding: utf-8 -*-
 #读入log文件，并写到新文件中
 from __future__ import print_function
-with open('gc.log','r') as f:
-  array=[]
+def extract():
+  print('OK')
+  with open('data/gc.log','r') as f:
+    array=[]
+    with open('data/newgc.log','w') as f1:
+      for line in f:
+        if line.find('concurrent-sweep-start]')>-1:
+          array.append(line)
+          f1.write(line)
+
+def daily():
   i=0
-  with open('newgc.log','w') as f1:
-    for line in f:
-      if line.find('concurrent-sweep-start]')>-1:
-        array.append(line)
-        f1.write(line)
+
+def monthly():
+  i=0
---
+++ testGcExtractor.py
@@ -0,0 +1,10 @@
+from gcExtractor import *
+
+def test_extract():
+  extract()
+
+def test_daily():
+  daily()
+
+def test_monthly():
+  monthly()
